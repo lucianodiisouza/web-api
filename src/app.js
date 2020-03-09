73bcb5b6f6fd7d12c548e8436ed48cca3a0c4a6c
Luciano dii Souza <contato@thecodelovers.com.br>
2020-03-09 03:05:48 -0300

    At this point i built the entry point of my application using Express (from npm) and have made some changes at my app.js file to support the format that Express works

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const http = require('http');
+const express = require('express');
+
+const app = express();
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Ola Mundo\n");
+app.set('port', port)
+
+app.use((req, res, next) => {
+    res.status(404).send();
 });
+
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
     console.log('Server is running...');
-})
+});
